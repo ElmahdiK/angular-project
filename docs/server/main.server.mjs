c86bad1273d236f177b45893a6f268c863367e2a
ElmahdiK <elmahdi.korfed@gmail.com>
2024-10-29 10:54:20 +0100

    feat(docs): ajout des fichiers build dans docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-54OABOTX.mjs";import"./chunk-HYL26NME.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MCTSSNOL.mjs";import"./chunk-K3ETOECX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
